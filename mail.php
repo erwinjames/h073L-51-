0af47b0838b75e309c8a5f1ecb64da4a1912ce34
erwinjames <manugasewinjames@gmail.com>
2024-04-08 07:36:22 +0800

    restoring

--- mail.php
+++ mail.php
@@ -1,64 +1,39 @@
 <?php
-require 'vendor/autoload.php';
-require 'lib/openpgp.php';
-
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// Create an instance of PHPMailer
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
-    // Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail->isSMTP();
-    $mail->Host       = 'mail.hotelasiacebu.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'it@hotelasiacebu.com';
-    $mail->Password   = 'sTxGLCy+ciHH';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port       = 465;
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'mail.hotelasiacebu.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'it@hotelasiacebu.com';                     //SMTP username
+    $mail->Password   = 'sTxGLCy+ciHH';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    // Recipients
+    //Recipients
     $mail->setFrom('it@hotelasiacebu.com', 'testing');
-    $mail->addAddress('manugasewinjames@gmail.com', 'Erwin James Test');
+    $mail->addAddress('manugasewinjames@gmail.com', 'Erwin James Test');     //Add a recipient
 
-    // Content
-    $mail->isHTML(true);
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    // Define email body
-    $html_body = 'This is the HTML message body <b>in bold!</b>';
-    $text_body = 'This is the body in plain text for non-HTML mail clients';
-
-    // Read the recipient's public key from the .asc file
-    $public_key_file = 'secret_room/KUNIO_0x50A57360_public.asc';
-    $public_key = file_get_contents($public_key_file);
-
-    // Encrypt the email content using the recipient's public key
-    $encrypted_html_body = encryptEmailContent($html_body, $public_key);
-    $encrypted_text_body = encryptEmailContent($text_body, $public_key);
-
-    // Include encrypted content in the email body
-    $mail->Body = $encrypted_html_body . '<br>' . $encrypted_text_body;
-
-    // Send the email
     $mail->send();
     echo 'Message has been sent';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
-// Function to encrypt email content using the recipient's public key
-function encryptEmailContent($content, $public_key) {
-    // Parse the public key
-    $key = OpenPGP_Message::parse($public_key);
-
-    // Encrypt the content
-    $encrypted = $key->encrypt($content);
-
-    // Return the encrypted content
-    return $encrypted;
-}
-?>
