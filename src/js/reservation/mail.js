7253f42fc7dde8a33e66f7566278d7a8ec269f71
erwinjames <manugasewinjames@gmail.com>
2024-04-08 08:05:49 +0800

    udpated

---
+++ src/js/reservation/mail.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#submit_btn').click(function() {
+      var form = $('#kt_create_reservation_form')[0];
+      var formData = new FormData(form);
+      $.ajax({
+        url: '../../modules/mail.php',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response) {
+          console.log(response);
+        },
+        error: function(xhr, status, error) {
+          console.error('Request failed. Status: ' + status);
+        }
+      });
+    });
+  });
