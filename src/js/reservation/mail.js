20994a67f48fa01c82f98b87ead6d43cf5b6c641
erwinjames <manugasewinjames@gmail.com>
2024-04-11 08:33:27 +0800

    update

--- src/js/reservation/mail.js
+++ src/js/reservation/mail.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
       var form = $('#kt_create_reservation_form')[0];
       var formData = new FormData(form);
       $.ajax({
-        url: '/src/modules/mail.php',
+        url: 'modules/mail.php',
         type: 'POST',
         data: formData,
         processData: false,
